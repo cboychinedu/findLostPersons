27ca39b43f5a40d77ec28281fb90881d84191ab3
Mbonu Chinedum <cboy.chinedu@gmail.com>
2025-09-22 20:44:59 +0100

    working on the train network page

--- myapp/src/Pages/TrainNeuralNetwork/TrainNetwork.jsx
+++ myapp/src/Pages/TrainNeuralNetwork/TrainNetwork.jsx
@@ -2,6 +2,7 @@
 import React, { Fragment, useEffect, useRef, useState  } from 'react'; 
 import DashboardNavbar from '../../Components/Navbar/DashboardNavbar';
 import Footer from '../../Components/Footer/Footer';
+import trainImageDisplay from "../../Images/trainImage.jpg"; 
 
 // Creating the TrainNeuralNetwork component 
 const TrainNeuralNetwork = () => {
@@ -34,19 +35,29 @@ const TrainNeuralNetwork = () => {
 
             {/* Adding the main div  */}
             <div className="container min-h-screen p-30 mx-auto my-10 mt-[110px] mb-[300px]">
-                <h1 className="text-3xl font-bold mb-4">Train Neural Network</h1>
-                <p className="mb-4">This is where you can train the machine's Neural Network model on the <b className="text-[25px] ml-[7px]"> missing person's image.  </b>  <br/> 
-                    <ol className="mt-[20px] list-decimal list-inside leading-[40px] "> 
-                        <span className=" mb-[50px]"> Firstly, get the following: </span>
-                        <li> An image of the missing person. </li>
-                        <li> The labeled annotations for the image in either Pascal VOC XML format or YOLO TXT format.</li>
-                        <li> Upload the image and the corresponding labeled annotations using the provided file input fields below.</li>
-                        <li> Click the "<b> Train Model </b>" button to initiate the training process.</li>
-                        <li> The system will process the uploaded data and train the neural network model accordingly.</li>
-                        <li> Once the training is complete, you will receive a notification indicating that the model has been successfully trained.</li>
-                        <li> You can then use the trained model for object detection tasks related to missing persons.</li>
-                    </ol>
-                </p>
+                <div className="flex justify-center mb-[50px]">
+                    <div> 
+                        <h1 className="text-3xl font-bold mb-4">Train Neural Network</h1>
+                        <p className="mb-4">This is where you can train the machine's Neural Network model on the <b className="text-[25px] ml-[7px]"> missing person's image.  </b>  <br/> 
+                            <ol className="mt-[20px] list-decimal list-inside leading-[40px] "> 
+                                <span className=" mb-[50px]"> Firstly, get the following: </span>
+                                <li> An image of the missing person. </li>
+                                <li> The labeled annotations for the image in either Pascal VOC XML format or YOLO TXT format.</li>
+                                <li> Upload the image and the corresponding labeled annotations using the provided file input fields below.</li>
+                                <li> Click the "<b> Train Model </b>" button to initiate the training process.</li>
+                                <li> The system will process the uploaded data and train the neural network model accordingly.</li>
+                                <li> Once the training is complete, you will receive a notification indicating that the model has been successfully trained.</li>
+                                <li> You can then use the trained model for object detection tasks related to missing persons.</li>
+                            </ol>
+                        </p>
+                    </div> 
+
+                    <div className="w-[50%] px-[30px] pl-[100px] py-[30px]"> 
+                        <img src={trainImageDisplay} alt="Train Neural Network" className="w-[100%] h-[65%] rounded-lg mt-[20px] mb-[20px]"/>
+                    </div>
+                    
+                </div>
+
                 
                 {/* Add your image for training the model here */}
                 <div className=''>
