bfcd887ddbbe30b4362a7a26732e603515e0ef89
cboy.chinedu <cboy.chinedu@gmail.com>
2025-10-21 10:16:17 +0100

    Working the bash script

--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@
 # Starting the programs for finding the lost persons 
 echo "Hello, welcome to the Find Lost Persons project!"
 
+# Listing to see if any process is runnin 
+pm2 list 
+
 # Start backend
 echo "Starting backend..."
 cd backend/ 
-pm2 start dist/app.js --name "backendApp" 
+pm2 start dist/app.js --name "backend" 
 
 # Change to the project directory 
 cd ../ 
@@ -15,11 +18,17 @@ cd ../
 echo "Starting Machine learning server..."
 cd mlServer/
 
-# Activating the ml environment 
-conda activate ml 
+# Installing the required dep 
+echo "Installing the required dep"
+pip install -r requirements.txt 
 
 # Starting the machine learing applications 
-pm2 start "app.py" --name "mlServerApp" --interpreter=python
+# Switching the environments 
+echo "Switch environments" 
+conda activate ml 
+
+echo "Starting the python application" 
+pm2 start "app.py" --name "mlServer" --interpreter=python
 
 # Change to the project directory 
 cd ../
@@ -32,7 +41,7 @@ cd myapp/
 npm run build 
 
 # Starting the web application 
-pm2 serve build/ 3000 --name "frontendApp" --spa
+pm2 serve build/ 3000 --name "frontend" --spa
 
 # All services started 
 echo "All services started."; 
